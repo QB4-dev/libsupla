a88e1e26f6d016de92c6bcadeed860ff74b452ae
kuba <qb4.dev@gmail.com>
2022-12-23 11:19:52 +0100

    tests updated

--- test/test_channel.c
+++ test/test_channel.c
@@ -5,50 +5,35 @@
 
 #include <libsupla/channel.h>
 
-static supla_channel_t test_channel = {
-	.type = SUPLA_CHANNELTYPE_THERMOMETER ,
-	.supported_functions = SUPLA_CHANNELFNC_THERMOMETER ,
-	.default_function = SUPLA_CHANNELFNC_THERMOMETER,
-};
-
+static supla_channel_t *temp_channel;
 
 void setUp(void)
 {
-	TEST_ASSERT_NULL(test_channel.priv);
-	TEST_ASSERT_EQUAL(SUPLA_RESULT_TRUE,supla_channel_init(&test_channel));
+	supla_channel_config_t temp_channel_config = {
+		.type = SUPLA_CHANNELTYPE_THERMOMETER ,
+		.supported_functions = SUPLA_CHANNELFNC_THERMOMETER,
+		.default_function = SUPLA_CHANNELFNC_THERMOMETER,
+	};
+
+	temp_channel = supla_channel_create(&temp_channel_config);
+	TEST_ASSERT_NOT_NULL(temp_channel);
 }
 
 void tearDown(void)
 {
-	TEST_ASSERT_EQUAL(SUPLA_RESULT_TRUE,supla_channel_deinit(&test_channel));
-	TEST_ASSERT_NULL(test_channel.priv);
-}
-
-void test_channel_init(void)
-{
-	supla_channel_t channel = {
-		.type = SUPLA_CHANNELTYPE_THERMOMETER ,
-		.supported_functions = SUPLA_CHANNELFNC_THERMOMETER ,
-		.default_function = SUPLA_CHANNELFNC_THERMOMETER,
-	};
-
-    TEST_ASSERT_NULL(channel.priv);
-    TEST_ASSERT_EQUAL(SUPLA_RESULT_TRUE,supla_channel_init(&channel));
-    TEST_ASSERT_EQUAL(SUPLA_RESULT_TRUE,supla_channel_deinit(&channel));
-    TEST_ASSERT_NULL(channel.priv);
+	TEST_ASSERT_EQUAL(SUPLA_RESULT_TRUE,supla_channel_free(temp_channel));
 }
 
 void test_channel_get_assigned_number(void)
 {
-	TEST_ASSERT_EQUAL_INT(-1,supla_channel_get_assigned_number(&test_channel));
+	TEST_ASSERT_EQUAL_INT(-1,supla_channel_get_assigned_number(temp_channel));
 }
 
 void test_channel_get_active_function(void)
 {
 	int active_functions = 0x00;
 
-	TEST_ASSERT_EQUAL_INT(SUPLA_RESULT_FALSE,supla_channel_get_active_function(NULL,0x00));
-	TEST_ASSERT_EQUAL_INT(SUPLA_RESULT_TRUE,supla_channel_get_active_function(&test_channel,&active_functions));
+	TEST_ASSERT_EQUAL_INT(SUPLA_RESULT_TRUE,supla_channel_get_active_function(temp_channel,&active_functions));
 }
 
 #endif // TEST
--- test/test_device.c
+++ test/test_device.c
@@ -12,31 +12,38 @@ static supla_dev_t *dev;
 
 void setUp(void)
 {
-    dev = supla_dev_create(DEV_NAME,SOFT_VER);
+	dev = supla_dev_create(DEV_NAME,SOFT_VER);
 }
 
 void tearDown(void)
 {
-    supla_dev_free(dev);
-}
-
-void test_device_init(void)
-{
-    TEST_ASSERT_NOT_NULL(dev);
+	supla_dev_free(dev);
 }
 
 void test_device_functions(void)
 {
 	char buf[32] = {};
 
-    TEST_ASSERT_NOT_NULL(dev);
+	TEST_ASSERT_NOT_NULL(dev);
 
-    TEST_ASSERT_EQUAL(SUPLA_RESULT_TRUE,supla_dev_get_name(dev,buf,sizeof(buf)));
-    TEST_ASSERT_EQUAL_STRING_MESSAGE(DEV_NAME,buf,"wrong device name");
+	TEST_ASSERT_EQUAL(SUPLA_RESULT_TRUE,supla_dev_get_name(dev,buf,sizeof(buf)));
+	TEST_ASSERT_EQUAL_STRING_MESSAGE(DEV_NAME,buf,"wrong device name");
 
-    TEST_ASSERT_EQUAL(SUPLA_RESULT_TRUE,supla_dev_get_software_version(dev,buf,sizeof(buf)));
-    TEST_ASSERT_EQUAL_STRING_MESSAGE(SOFT_VER,buf,"wrong software version");
+	TEST_ASSERT_EQUAL(SUPLA_RESULT_TRUE,supla_dev_get_software_version(dev,buf,sizeof(buf)));
+	TEST_ASSERT_EQUAL_STRING_MESSAGE(SOFT_VER,buf,"wrong software version");
 
+}
+
+void test_device_config(void)
+{
+	const struct supla_config supla_config = {
+		.email = "user@example.com",
+		.auth_key = {0xFF,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00},
+		.guid = {0xFF,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00},
+		.server = "svr.supla.org",
+	};
+
+	TEST_ASSERT_EQUAL(SUPLA_RESULT_TRUE,supla_dev_set_config(dev,&supla_config));
 }
 
 #endif // TEST
